18349b0ee2675d7d4fbe6e95fd0e45f4e9b27c17
rebeka <rebeka.dea.l@gmail.com>
2022-11-02 14:55:23 +0100

    Install dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,12 +13,22 @@ module.exports = {
       lg: '1200px',
       xl: '1536px',
     },
-    fontFamily: {
-      logo: ['Bebas Neue'],
-    },
-    height: {
-      banner: '500px',
-      searchInput: '36px',
+    extend: {
+      fontFamily: {
+        logo: ['Bebas Neue'],
+      },
+      height: {
+        banner: '500px',
+        searchInput: '36px',
+        card: '200px',
+      },
+      borderRadius: {
+        default: '10px',
+        full: '100%',
+      },
+      dropShadow: {
+        custom: '0px 10px 30px rgba(175, 198, 245, 0.2)',
+      },
     },
   },
   plugins: [],
