2185c10a99f6162c5355a875f6758d561cdf5585
rebeka <rebeka.dea.l@gmail.com>
2022-11-02 14:57:16 +0100

    add navigation header

--- components/navigation/header/Header.tsx
+++ components/navigation/header/Header.tsx
@@ -3,32 +3,56 @@ import Search from '../../utility/search/Search';
 
 export interface IHeader extends React.ComponentPropsWithoutRef<'header'> {}
 
-const Header: React.FC<IHeader> = ({ className, ...headerProps }) => {
+const Header: React.FC<IHeader> = ({ ...headerProps }) => {
   return (
     <header
       {...headerProps}
-      className={`z-10 w-full flex flex-row justify-between ${className}`}
+      className={`w-full flex flex-row justify-between p-3`}
     >
-      <div className="space-x-5 m-5 flex align-middle">
+      <div className="space-x-7 flex">
         <Link href="/">
-          <a className="font-logo text-3xl leading-none">Watchflix</a>
-        </Link>
-        <Link href="/">
-          <a className="hover:font-bold">
-            <p>Home</p>
+          <a className="font-logo text-3xl flex  items-center">
+            <p className=" leading-none logo-text">Watchflix</p>
           </a>
         </Link>
-        <Link href="/">
-          <a className="hover:font-bold">Genres</a>
-        </Link>
+        <div className=" hidden sm:flex space-x-5">
+          <Link href="/">
+            <a className="hover:font-bold flex  items-center">
+              <p className="leading-none ">Home</p>
+            </a>
+          </Link>
+          <Link href="/genres" className="flex">
+            <a className="hover:font-bold  flex  items-center">
+              <p className="leading-none ">Genres</p>
+            </a>
+          </Link>
+        </div>
       </div>
-      <div className="space-x-5 m-5 flex">
-        <Search />
-        <Link href="/">
-          <a>
+      <div className="">
+        <div className="space-x-5  sm:flex  hidden">
+          <Search />
+          <div className="dropdown relative">
             <div className="h-searchInput w-9 bg-blue-600 rounded-full"></div>
-          </a>
-        </Link>
+            <ul className="dropdown-menu absolute hidden right-0 text-gray-700 pt-1 w-max z-50">
+              <li className="">
+                <Link
+                  className="rounded-t-default bg-gray-200 hover:bg-purple-300 py-2 px-4 block whitespace-no-wrap"
+                  href="/account"
+                >
+                  Account Settings
+                </Link>
+              </li>
+              <li className="">
+                <a
+                  className="rounded-b-default bg-gray-200 hover:bg-purple-300 py-2 px-4 block whitespace-no-wrap"
+                  href="#"
+                >
+                  Sign Out
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </header>
   );
